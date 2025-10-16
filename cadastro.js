d7e2f9b38c923acf7fd90a9a9ae3e9ef0dbb468e
joaopaulogomes731 <joaopaulogomes731@gmail.com>
2025-10-16 19:23:22 +0000

    start database

--- cadastro.js
+++ cadastro.js
@@ -3,6 +3,7 @@ const registerPasswordInput = document.getElementById('registerPassword');
 const registerBtn = document.getElementById('registerBtn');
 
 const auth = firebase.auth();
+const database = firebase.database();
 
 registerBtn.addEventListener('click', () => {
     const email = registerEmailInput.value;
@@ -10,8 +11,18 @@ registerBtn.addEventListener('click', () => {
 
     auth.createUserWithEmailAndPassword(email, password)
         .then((userCredential) => {
-            alert('Usuário registrado com sucesso!');
-            window.location.href = 'login.html';
+            const user = userCredential.user;
+            // Save user data to the database
+            database.ref('users/' + user.uid).set({
+                email: email
+            })
+            .then(() => {
+                alert('Usuário registrado com sucesso!');
+                window.location.href = 'login.html';
+            })
+            .catch((error) => {
+                alert('Erro ao salvar dados do usuário: ' + error.message);
+            });
         })
         .catch((error) => {
             const errorMessage = error.message;
